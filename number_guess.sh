eb7fd2512cdeb13a44ad28a9f30bcf9de97c8c1a
Tainã Martinez <tai.souza.martinez@gmail.com>
2024-07-14 21:17:16 +0000

    feat: check if user exists

--- number_guess.sh
+++ number_guess.sh
@@ -3,3 +3,16 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_ID ]]; then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE user_id=$USER_ID")
+  echo $USER_INFO | while IFS="|" read GAMES_PLAYED BEST_GAME; do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
